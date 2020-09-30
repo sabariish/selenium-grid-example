10fbfab68dec2e8d2748d9d642f270dd05c6ae23
Sabarish Sasidharan <sabarish.sasidharan@gmail.com>
2020-09-29 20:14:19 -0700

    Add files via upload

---
+++ src/test/java/auto/BrowserTest2.java
@@ -0,0 +1,68 @@
+package auto;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+
+public class BrowserTest2 extends WebDriverTestCase{
+
+	
+@Test()
+public void test () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+@Test()
+public void test2 () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+@Test()
+public void test3 () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+@Test()
+public void test4 () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+@Test()
+public void test5 () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+@Test()
+public void test6 () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+@Test()
+public void test7 () {
+	
+	getDriver().get("https:\\www.google.com");
+	getDriver().findElement(By.name("q")).verifyPresent();
+	getDriver().quit();
+	
+}
+	
+}
